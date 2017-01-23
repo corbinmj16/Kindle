8b7955d01191c64a217d174b45a0c525a0014707
Corbin <corbinmjensen@gmail.com>
2017-01-22 20:01:26 -0500

    First video in Kindle course

--- Kindle/ViewController.swift
+++ Kindle/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
+class Book {
+    let title: String
+    let author: String
+    let pages: [Page]
+    
+    init(title: String, author: String, pages: [Page]) {
+        self.title = title
+        self.author = author
+        self.pages = pages
+    }
+}
+
+class Page {
+    let pageNum: Int
+    let text: String
+    
+    init(pageNum: Int, text: String) {
+        self.pageNum = pageNum
+        self.text = text
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .red
+        
+        let page = Page(pageNum: 1, text: "Text for the first page of the Steve Jobs book")
+        let page2 = Page(pageNum: 2, text: "Second page on Steve Jobs books")
+        print(page.pageNum)
+        print(page2.text)
+        
+//        let book = Book(title: "Steve Jobs", author: "Walter Issacson")
+//        print("Title: \(book.title), Author: \(book.author)")
+        
+        let stevePages = [page, page2]
+        
+        let steveBook = Book(title: "Steve Jobs", author: "Walter Issacson", pages: stevePages)
+        print("Title: \(steveBook.title), Author: \(steveBook.author), Pages: \(steveBook.pages[0].text)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
