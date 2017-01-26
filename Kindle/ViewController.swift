5bf4729637806b4f2d28125717864d0bbe224320
Corbin Jensen <cjensen@carbonhouse.com>
2017-01-25 23:34:08 -0500

    create optional to hold books

--- Kindle/ViewController.swift
+++ Kindle/ViewController.swift
@@ -31,35 +31,56 @@ class Page {
 }
 
 class ViewController: UIViewController {
+    // create optional property
+    // books is an array of type Book
+    var books : [Book]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
         
+        // code below here
+        setupBooks()
+        
+    }
+    
+    func setupBooks() {
         let page = Page(pageNum: 1, text: "Text for the first page of the Steve Jobs book")
         let page2 = Page(pageNum: 2, text: "Second page on Steve Jobs books")
-
         
         let stevePages = [page, page2]
         
         let steveBook = Book(title: "Steve Jobs", author: "Walter Issacson", pages: stevePages)
         let billBook = Book(title: "Bill Gates", author: "Michael P.", pages: [
-                Page(pageNum: 1, text: "Text for page 3 of Bill"),
-                Page(pageNum: 2, text: "Second Page of Bill"),
-                Page(pageNum: 3, text: "Third Page of book of bill"),
-                Page(pageNum: 4, text: "Fourth page player")
+            Page(pageNum: 1, text: "Text for page 3 of Bill"),
+            Page(pageNum: 2, text: "Second Page of Bill"),
+            Page(pageNum: 3, text: "Third Page of book of bill"),
+            Page(pageNum: 4, text: "Fourth page player")
             ])
         
-        for book in [steveBook, billBook] {
-            print("Title: \(book.title)")
-            for page in book.pages {
-                print("Page \(page.pageNum): \(page.text)")
+        self.books = [steveBook, billBook]
+        
+        if let books = self.books {
+            for book in books {
+                print("Title: \(book.title)")
+                for page in book.pages {
+                    print("Page \(page.pageNum): \(page.text)")
+                }
             }
         }
         
-//        for page in steveBook.pages + billBook.pages {
-//            print(page.text)
-//        }
+        // not the safest way to unwrap optional
+        //        for book in self.books! {
+        //            print("Title: \(book.title)")
+        //            for page in book.pages {
+        //                print("Page \(page.pageNum): \(page.text)")
+        //            }
+        //        }
     }
 }
+
+
+
+
+
